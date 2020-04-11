4aa9017ce46ec85565700e275524a2744a52f75d
Lior Lavon <>
2020-04-11 18:06:56 +0200

    Update Vet controller to show a list with sorting. Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/petdataweb/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petdataweb/controllers/VetController.java
@@ -1,15 +1,39 @@
 package guru.springframework.petdataweb.controllers;
 
+import guru.springframework.petclinicdata.modules.Owner;
+import guru.springframework.petclinicdata.modules.Vet;
+import guru.springframework.petclinicdata.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"", "/index", "/index.html"})
-    public String showVets(){
+    private static VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String showVets(Model model){
+
+        Set<Vet> vets = vetService.findAll();
+
+        List<Vet> list = new ArrayList<>(vets);
+        Collections.sort(list, new Comparator<Vet>() {
+            @Override
+            public int compare(Vet o1, Vet o2) {
+                Long v = o1.getId() - o2.getId();
+                return v.intValue();
+            }
+        });
+
+        model.addAttribute("vets", list);
         return "vets/index";
     }
 }
