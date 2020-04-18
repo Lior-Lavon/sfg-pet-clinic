e848f31c2b953a16b7c6bc3e856dab8baf834b31
Lior Lavon <>
2020-04-18 11:27:46 +0200

    refactor development repository

--- pet-clinic-data/src/main/java/guru/springframework/petclinicdata/jpaImp/repositories/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.petclinicdata.jpaImp.repositories;
-
-import guru.springframework.petclinicdata.modules.Pet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PetRepository extends CrudRepository<Pet, Long> {
-}
--- pet-clinic-data/src/main/java/guru/springframework/petclinicdata/jpaImp/repositories/VetRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.petclinicdata.jpaImp.repositories;
-
-import guru.springframework.petclinicdata.modules.Vet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VetRepository extends CrudRepository<Vet, Long> {
-}
