4f1e1c55828262eb55020d33b63350517d73bcd8
Lior Lavon <>
2020-04-24 17:34:23 +0200

    CircleCI test-9

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/map/OwnerMapServiceTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.map;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
