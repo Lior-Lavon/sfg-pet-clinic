45696aa7344cce89942a1e4a8cc816b9ccf6c7f5
Lior Lavon <>
2020-04-24 19:05:17 +0200

    CircleCI test-105

--- pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/map/TestUserTest.java
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/map/TestUserTest.java
@@ -1,15 +1,37 @@
 package guru.springframework.sfgpetclinic.map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestUserTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
     @Test
     void getLastName() {
+    }
 
-        String name = "lior";
-        assertEquals("lior", name);
+    @Test
+    void setLastName() {
     }
 }
