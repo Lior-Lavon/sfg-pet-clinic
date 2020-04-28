801763f0fa98d3caee5fe356f786d7497c4997a2
Lior Lavon <>
2020-04-28 19:03:57 +0200

    fixing furfire plugin -4

--- pet-clinic-web/src/main/java/guru/springframework/petdataweb/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petdataweb/controllers/OwnerController.java
@@ -1,12 +1,7 @@
 package guru.springframework.petdataweb.controllers;
 
-import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.*;
 
 @RequestMapping("/owners")
 @Controller
--- pet-clinic-web/src/main/java/guru/springframework/petdataweb/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petdataweb/controllers/VetController.java
@@ -1,14 +1,7 @@
 package guru.springframework.petdataweb.controllers;
 
-import guru.springframework.sfgpetclinic.model.Vet;
-import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.*;
-
-//@RequestMapping("/vets")
 @Controller
 public class VetController {
 /*
